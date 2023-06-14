87d042a94e5e1dc8e684a94108c3e68d07c163bd
Caio <atsocs.cc@gmail.com>
2023-06-14 18:28:36 -0300

    refactor

--- src/lab3.jl
+++ src/lab3.jl
@@ -1,14 +1,11 @@
-function transmission_rate(N, dv, dc)
-    M = N * dv / dc
-    info_length = N
-    transmission_length = N + M
-    R = info_length / transmission_length
-    return R
+module LDPC
+
+transmission_rate(dv, dc) = dc / (dc + dv)
+
+# function that can design the parity check matrix for arbitrary values of
+# dv, dc and N.
+function design(dv, dc, N)
+
 end
 
-function transmission_rate(dv, dc)
-    return dc / (dc + dv)
 end
-
-# Queremos taxa = 4 / 7. Logo, queremos dc = 4 e dv = 3.
-# N / (N + M) = N / (N + N * dv / dc) = 1 / (1 + dv / dc) = dc / (dc + dv)
